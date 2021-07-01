9a32da7bd01b5c8a4777dc10c3174be3197f14c2
Chris Pak <cpak90@gmail.com>
2021-07-01 16:08:21 -0400

    change schema for docker

--- database/schemas/schema.sql
+++ database/schemas/schema.sql
@@ -29,17 +29,17 @@ CREATE TABLE photos (
 );
 
 COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM '/Users/chrispak/Downloads/questions.csv'
+FROM '/questions.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM '/Users/chrispak/Downloads/answers.csv'
+FROM '/answers.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY photos(id, answer_id, url)
-FROM '/Users/chrispak/Downloads/answers_photos.csv'
+FROM '/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
 
